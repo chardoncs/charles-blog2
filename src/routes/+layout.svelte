a537ea670286e89fbc1452c51409ef2fc3d1dea1
Charles Dong <chardon_cs@proton.me>
2024-11-11 16:51:22 +0800

    Add shadcn-svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+	import '../app.css'
+  import "@fontsource/fira-sans"
+  import { ModeWatcher } from "mode-watcher"
+
+	let { children } = $props()
 </script>
 
+<svelte:head>
+  <title>Charles</title>
+</svelte:head>
+
+<ModeWatcher defaultMode="system" />
 {@render children()}
