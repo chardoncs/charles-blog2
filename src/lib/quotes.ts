992a1b7b8df683daae7b69b3ea6abec5ea25d684
Charles Dong <chardon_cs@proton.me>
2024-11-12 10:48:40 +0800

    Update quotes

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -5,8 +5,10 @@ export const ferrisQuotePool = [
   "iykyk lol",
   ".unwrap()",
   "I play X4: Foundations btw",
+  "\"You guys using Java?\"",
   "\"You guys using Zig?\"",
   "Crablang",
   "\"All your codebase belong to us.\"",
   "**Powered by SvelteKit & Bun**",
+  "**Arrow made with Inkscape**",
 ]
