e6b7825edc2533ed9ebe486dc501f73a899093e0
Charles Dong <chardon_cs@proton.me>
2024-11-18 00:47:50 +0800

    Add cowsay components

---
+++ src/routes/cowsay/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import "@fontsource-variable/fira-code"
+  import "./page.css"
+  import { Card, CardContent } from "$lib/components/ui/card"
+  import { onMount } from "svelte";
+  import * as cowsay from "cowsay"
+  import { getRandomQuote } from "$lib/quotes"
+
+  let output = $state("")
+
+  onMount(() => {
+    output = cowsay.say({
+      text: getRandomQuote(),
+    })
+  })
+  
+</script>
+
+<Card>
+  <CardContent class="code-block">
+    {#each output.split("\n") as line }
+      <div>{line}</div>
+    {/each}
+  </CardContent>
+</Card>
