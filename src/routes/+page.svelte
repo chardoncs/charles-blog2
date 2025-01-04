ba4495062b2058d322d255aa2327e19ce31a5b70
Charles Dong <chardon_cs@proton.me>
2025-01-04 21:05:15 +0800

    Add repo link button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import NameCard from "$lib/components/name-card.svelte"
+    import { Button } from "$lib/components/ui/button";
   import UnsafeRust from "$lib/components/unsafe-rust.svelte"
+    import { GitBranchIcon } from "lucide-svelte";
 </script>
 
 <svelte:head>
@@ -18,6 +20,19 @@
   <div class="container !max-w-screen-md flex flex-col items-center">
     <NameCard />
   </div>
-
-  <UnsafeRust />
 </div>
+
+<footer>
+  <UnsafeRust />
+
+  <Button
+    class="absolute bottom-0 left-0"
+    variant="outline"
+    href="https://github.com/chardoncs/charles-blog2"
+    target="_blank"
+    size="icon"
+    title="Repository"
+  >
+    <GitBranchIcon class="size-4" />
+  </Button>
+</footer>
