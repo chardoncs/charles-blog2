34469198461e6bac6799f83c6ef2d2de8129a28b
Charles Dong <chardon_cs@proton.me>
2025-02-24 09:54:16 +0800

    Update intro article

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ const server = serve({
       { path: "images/icons/x.svg", mime: "image/svg+xml" },
       { path: "images/icons/mastodon.svg", mime: "image/svg+xml" },
       { path: "files/cydong-cv_v3.pdf", mime: "application/pdf" },
+      { path: "images/orcid.svg", mime: "image/svg+xml" },
     ),
   },
   development: Bun.env.DEBUG === "1",
